ccb7b4cd0950c9c69246342c03473493014eaf01
Siddhartha Ganguli <siddharthaganguli0093@gmail.com>
2026-01-03 00:58:21 +0530

    merge remote changes

--- src/model/main.py
+++ src/model/main.py
@@ -2,14 +2,15 @@ from training import Trainer
 from decision_tree_train import DecisionTreeModel
 
 SPLIT_DATA_PATH = "/Users/siddharthaganguli/Desktop/irish/data/split_data"
-MODEL_OUTPUT_PATH = "/Users/siddharthaganguli/Desktop/irish/irish_models"
+MODEL_OUTPUT_PATH = "/Users/siddharthaganguli/Desktop/irish/src/app/irishmodel/lrmodel"
+MODEL_OUTPUT_PATH2 = "/Users/siddharthaganguli/Desktop/irish/src/app/irishmodel/dtmodel"
 
 def main_training():
     trainer = Trainer(data_path=SPLIT_DATA_PATH, output_path=MODEL_OUTPUT_PATH)
     trainer.train_with_mlflow()
 
 def decision_tree():
-    decision_tree_model=DecisionTreeModel(data_path=SPLIT_DATA_PATH,output_path=MODEL_OUTPUT_PATH)
+    decision_tree_model=DecisionTreeModel(data_path=SPLIT_DATA_PATH,output_path=MODEL_OUTPUT_PATH2)
     decision_tree_model.train_with_mlflow()
 
 if __name__ == "__main__":
